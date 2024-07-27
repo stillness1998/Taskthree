cd3069f185665c43fd9d1ee0bdb690a2aaebdd3a
stillness1998 <138491851+stillness1998@users.noreply.github.com>
2024-07-27 17:19:44 +0530

    Add files via upload

---
+++ hello_php.php
@@ -0,0 +1,6 @@
+<?php
+$name = "Manu";
+$custom_date = "2024-07-27";
+$current_time = date("H:i:s");
+echo "Hello, $name, right now the time is $current_time on $custom_date";
+?>
---
+++ hello_rust.rs
@@ -0,0 +1,6 @@
+fn main() {
+    use std::time::Instant;
+
+    let now = Instant::now();
+    println!("Hello, Manu! The current time is {:?}", now);
+}
